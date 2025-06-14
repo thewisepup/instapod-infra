244087556a9795e8fd64c3ceddcef73191149aad
thewisepup <wisepup257@gmail.com>
2025-06-14 15:26:18 -0700

    create podcast-generate-lambda-stack ecr repo

--- __main__.py
+++ __main__.py
@@ -1,12 +1,18 @@
 """An AWS Python Pulumi program"""
 
 import pulumi
-from pulumi_aws import s3
+import pulumi_aws as aws
 
 stack = pulumi.get_stack()
 
+
+podcast_generation_image = aws.ecr.Repository(
+    f"podcast-generator-lambda-{stack}",
+    name=f"podcast-generator-lambda-{stack}",
+    image_tag_mutability="MUTABLE",
+)
 # Create an AWS resource (S3 Bucket)
-bucket = s3.BucketV2(f"my-bucket-{stack}")
+bucket = aws.s3.BucketV2(f"my-bucket-{stack}")
 
 # Export the name of the bucket
 pulumi.export(f"my-bucket-{stack}", bucket.id)
